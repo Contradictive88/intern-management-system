02b935df5ca8fa7017d3f3d2d03fceff61ea19fa
Contradictive88 <lanceorville5@gmail.com>
2024-05-31 17:15:26 +0800

    updated sidebar and header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { BiArrowToLeft, BiSolidUserCircle } from "react-icons/bi";
 
 const Header: React.FC = () => {
     return (
-        <header className="bg-gray-800 text-white p-4 text-center">
-            <h1>My App</h1>
+        <header className="bg-gray-100 p-7 text-center flex justify-between">
+            <BiArrowToLeft className="text-4xl" />
+            <div>
+                <BiSolidUserCircle className="text-4xl" />
+            </div>
         </header>
     );
 };
--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import { BiSolidUser, BiSolidTrophy, BiLogOut,BiSolidCube } from "react-icons/bi";
+import { RiPagesFill } from "react-icons/ri";
+import { IoMdSettings } from "react-icons/io";
 
 const Sidebar: React.FC = () => {
     return (
-        <aside className="w-64 bg-gray-100 p-4 h-full">
-            <nav>
-                <ul>
-                    <li><a href="/profile" className="block py-2">Profile</a></li>
-                    <li><a href="/settings" className="block py-2">Settings</a></li>
-                    <li><a href="/logout" className="block py-2">Logout</a></li>
+        <aside className="w-64 bg-gray-800 text-white px-4 h-full flex flex-col">
+            <h2 className="font-semibold py-5 my-2 flex items-center"><BiSolidCube className="mr-2 text-5xl" />Intern Management System</h2>
+            <nav className="flex-1">
+                <ul className="mt-4">
+                    <li className="flex items-center p-4 mb-2 rounded-md bg-white text-gray-800">
+                        <BiSolidUser className="mr-2 text-2xl" />
+                        <a href="/profile" className="block text-lg">Profile</a>
+                    </li>
+                    <li className="flex items-center p-4 mb-2 rounded-md">
+                        <RiPagesFill className="mr-2 text-2xl" />
+                        <a href="/report/daily" className="block text-lg">DTR</a>
+                    </li>
+                    <li className="flex items-center p-4 mb-2 rounded-md">
+                        <BiSolidTrophy className="mr-2 text-2xl" />
+                        <a href="/achievements" className="block text-lg">Achievements</a>
+                    </li>
+                    <li className="flex items-center p-4 mb-2 rounded-md">
+                        <IoMdSettings className="mr-2 text-2xl" />
+                        <a href="/settings" className="block text-lg">Settings</a>
+                    </li>
                 </ul>
             </nav>
         </aside>
